6f769f6c7958b740bcf4b250fd0b1856f29d2347
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-15 15:35:01 +0800

    Brew formula update for giter version v0.0.4

--- Formula/giter.rb
+++ Formula/giter.rb
@@ -1,9 +1,9 @@
 class Giter < Formula
   desc "Git users manager"
   homepage "https://github.com/jsmartx/"
-  url "https://github.com/jsmartx/giter/releases/download/v0.0.3/giter_0.0.3_darwin_amd64.tar.gz"
-  version "0.0.3"
-  sha256 "7317bf14dcde8af4b310cd814d712d02f761c8990b5d3e5846cbf1819705be14"
+  url "https://github.com/jsmartx/giter/releases/download/v0.0.4/giter_0.0.4_darwin_amd64.tar.gz"
+  version "0.0.4"
+  sha256 "690f955869b6e95ce9f1b24802ca753657018c2567b53b1c95a172918695033a"
 
   def install
     bin.install "giter"
